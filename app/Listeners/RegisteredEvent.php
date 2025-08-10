6a0aab6b1fb3e6fecb125c2fe84e8a284f0b7d7f
Oliver Gomez <kayserenrique@gmail.com>
2025-08-10 09:30:10 -0400

    listener de registro de usuario mediante mediante formulario

---
+++ app/Listeners/RegisteredEvent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Auth\Events\Registered;
+
+class RegisteredEvent
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Registered $event): void
+    {
+        //Asigna el tipo de usuario al Registrarse
+        $event->user->assignRole('Usuario');
+    }
+}

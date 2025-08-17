b7b403499b71a4dc5efdedbe1bf633518084f048
Oliver Gomez <kayserenrique@gmail.com>
2025-08-17 11:12:59 -0400

    componentes para la landig page

---
+++ app/Livewire/Componentespage/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Componentespage;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('livewire.componentespage.header');
+    }
+}
